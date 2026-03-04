b214b96405de32e0a7efa45e4486a9a41ca6e6ad
david rocha <davrocha1006@gmail.com>
2026-03-04 11:08:29 -0300

    Create LoginUsuarioView.php

---
+++ view/LoginUsuarioView.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login de Usuário</title>
+</head>
+<body>
+
+    <h1>Acessar Sistema</h1>
+
+    <form action="../control/LoginUsuarioControl.php" method="POST">
+        
+        <label for="email">E-mail:</label><br>
+        <input type="email" name="email" id="email" required placeholder="Digite seu e-mail">
+        <br><br>
+
+        <label for="senha">Senha:</label><br>
+        <input type="password" name="senha" id="senha" required placeholder="Digite sua senha">
+        <br><br>
+
+        <button type="submit">Entrar</button>
+        <button type="reset">Limpar</button>
+
+    </form>
+
+    <hr>
+
+    <p>Ainda não tem conta? 
+        <a href="CadastrarUsuarioView.php">Cadastre-se aqui</a>
+    </p>
+    
+    <a href="../index.php">
+        <button>voltar</button>
+    </a>
+
+</body>
+</html>
